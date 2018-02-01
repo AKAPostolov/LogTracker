8dcf47d58dcaf8e14c329054d9e46d4074afd521
XTO <alexanderkirilovapostolov@hotmail.com>
2018-02-01 12:42:57 +0100

    Commit 2 and push

--- src/examples/Main.java
+++ src/examples/Main.java
@@ -27,10 +27,18 @@ public class Main
     static String arg1 = "";
     static String arg2 = "";
     static String arg3 = "";
+    public static void printReceivedArgs(String args[])
+    {
+        String commandLine = "";
+        for(int i=0; i<args.length; i++)
+        {
+            commandLine+=args[i]+" ";
+        }
+        System.out.println(commandLine);
+    }
      public static void main(String args[]) 
      {
-         String argument = "";
-         
+        printReceivedArgs(args);
         switch (args.length)
         {
             case 0:
@@ -74,7 +82,7 @@ public class Main
                 //-file c:\bat.bat -contains error atLast 3
                 kword1 = args[0]; //keyWORD: file
                 kword2 = args[2]; //keyWORD: contains
-                kword3 = args[4]; //keyWORD: existAt/atLine or at-Number/position/line-FromLast
+                kword3 = args[4]; //keyWORD: existAt/atLine or at-Number/position/line-FromLast or > >>
                 
                 arg1 = args[1]; //file
                 arg2 = args[3]; //string
@@ -89,12 +97,20 @@ public class Main
                   boolean result = isStringInFileLineFromLast(arg2, arg1, arg3);
                   System.out.println("result: " + result);
                 }
-            default:
+                else if(kword1.toLowerCase().contains("file") &&kword2.toLowerCase().contains("contains") && kword3.toLowerCase().contains(">"))
+                {
+                  boolean result = printMatchingStringsInFile(arg2, arg1, arg3);
+                  System.out.println("result: " + result);
+                }
                 break;
+            default:
+                System.out.println("Unhandled number or params, please check the help privided below ");
+                showHelp();
+            break;
         }
          System.out.println("End of process");
      }
-          public static boolean isStringInFileLine(String cadena, String filename, String lineNumber)
+     public static boolean isStringInFileLine(String cadena, String filename, String lineNumber)
      {
          try
          {
@@ -218,6 +234,33 @@ public class Main
             return 0;
          }
      }
+     public static boolean printMatchingStringsInFile(String cadena, String filename, String outputFilename)
+     {
+         try
+         {
+            List<String> lines = Files.readAllLines(Paths.get(filename), Charset.defaultCharset());
+            int i = 0; 
+            for (String line: lines)
+             {
+                 i++;
+                 if(line.toLowerCase().contains(cadena.toLowerCase()))
+                 {
+                     String printLine = "Result printMatchingStringsInFile: String found at line: " + i + "/" + lines.size();
+                     System.out.println(printLine);
+                     String outputLine = line + " @L"+i+"/"+lines.size();
+                     writeToFile(outputFilename,outputLine);
+                     return true;
+                 }
+             }
+            return false;
+         }
+         catch(Exception e)
+         {
+            System.out.println("Exception msg: " + e.getMessage());
+            System.out.println("Exception cause: " + e.getCause());
+            return false;
+         }
+     }
      public static void showInsufficientArgs()
      {
         System.out.println("WOW: Insufficient params");
@@ -225,24 +268,31 @@ public class Main
      }
      public static void showHelp()
      {
-        System.out.println("*****************************************************************************");
-        System.out.println("HELP:\n (How to use)");
-        System.out.println("-help (For help = THIS TEXT) \n Commands:");
-        System.out.println("-file param1 -contains param2");
-        System.out.println("-file param1 -countLines param2");
-        
-        System.out.println("-file c:\\bat.bat -contains error");
-        System.out.println("-file c:\\bat.bat -countLines error");
-        System.out.println("-file c:\\bat.bat -contains error atLine 7");
-        System.out.println("-file c:\\bat.bat -contains error atLast 3");
-
-        System.out.println("");
-        System.out.println(" '-' in front of a keyWord is not mandatory: search youtString method topdown");
-        System.out.println("*****************************************************************************");
+        System.out.println("*****************************************************");
+        System.out.println("*   .•°¤*(¯`★´¯)*¤° LogTracker °¤*(¯´★`¯)*¤°•. start");
+        System.out.println("* ");
+        System.out.println("*                    Commands: ");
+        System.out.println("* ");
+        System.out.println("* [ ] -help (after param error or empty param)");
+        System.out.println("* [1] -file c:\\file -contains error");
+        System.out.println("* [2] -file c:\\file -countLines error");
+        System.out.println("* [3] -file c:\\file -contains error atLine 7");
+        System.out.println("* [4] -file c:\\file -contains error atLast 3");
+        System.out.println("* [5] -file c:\\file -contains error > outputFilename");
+        System.out.println("* ");
+        System.out.println("* [ ] Explained:");
+        System.out.println("* [1] Returns true/false if a string matches in a file");
+        System.out.println("* [2] Returns number of lines matching the specified string");
+        System.out.println("* [3] Returns true/false if a string matches in a line");
+        System.out.println("* [4] Same as 3 but the line is specified downToUp");
+        System.out.println("* [5] Returns the number of lines and print these lines in a file");
+        System.out.println("* ");
+        System.out.println("*   .•°¤*(¯`★´¯)*¤° LogTracker °¤*(¯´★`¯)*¤°•. end");
+        System.out.println("***************************************************");
      }
      private static final String newLine = System.getProperty("line.separator");
 
-private synchronized void writeToFile(String fileName, String msg)  
+private static synchronized void writeToFile(String fileName, String msg)  
 {
     PrintWriter printWriter = null;
     File file = new File(fileName);
